c76cf16d8ec278b4f8e59700bebb9f61f6155c6e
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-06-18 18:23:36 +0200

    Test for secret

--- joke.go
+++ joke.go
@@ -9,6 +9,12 @@ func main() {
 
 	fmt.Println("Printing args given knock-knock:", os.Args[1:])
 
+	for _, v := range os.Args[1:] {
+		if v == "testtest" {
+			fmt.Println("BINGO!")
+		}
+	}
+
 	fmt.Println("Knock knock")
 	fmt.Println("Who's there?")
 	fmt.Println("Go")
