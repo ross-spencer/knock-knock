64c44e41b3c8eea3f69039a0aadee4144580f47a
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-06-18 20:09:09 +0200

    Env

--- joke.go
+++ joke.go
@@ -10,9 +10,16 @@ func main() {
 	fmt.Println("Printing args given knock-knock:", os.Args[1:])
 
 	for _, v := range os.Args[1:] {
+		fmt.Println("XXX", v)
 		if v == "testtest" {
 			fmt.Println("BINGO!")
 		}
+	}
+
+	if os.Getenv("SUPER_SECRET") == "testtest" {
+		fmt.Println("Oh! got it!")
+	} else {
+		fmt.Println("YYY", os.Getenv("SUPER_SECRET"))
 	}
 
 	fmt.Println("Knock knock")
